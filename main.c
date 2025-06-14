4e6dd2a739312bc5bd3a0ee497da651440f2c6c4
NocesAdrian <adriannoces1@gmail.com>
2025-06-14 12:54:04 +0800

    development

---
+++ main.c
@@ -0,0 +1,14 @@
+// ----------- headers ----------- //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <math.h>
+
+// ----- User Defined headers ----- //
+
+
+int main() {
+
+    return 0;
+}
